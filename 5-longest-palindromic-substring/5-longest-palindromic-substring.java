c6e8b41e9744e0fe19764e18d1f88629e4567217
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-09-07 10:09:42 +0530

    Time: 36 ms (69.88%), Space: 43.3 MB (56.86%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -31,59 +31,59 @@ class Solution {
         return str.substring(start, start + maxLength);
     }
     
-    private int[] oddLongestPalindrome(String s){
-        int maxi=1, m=1, l=0, r=2;
-        int[] ind = new int[]{0,0};
-        while(m<s.length()-1){
-            if(l<0 || r == s.length()){
-                m++;
-                l=m-1;
-                r=m+1;
-                continue;
-            }
-            if(s.charAt(l)==s.charAt(r)){
-                if(maxi<r-l+1){
-                    maxi = r-l+1;
-                    ind[0]=l;
-                    ind[1]=r;
-                }
-                l--;
-                r++;
-            }
-            else{
-                m++;
-                l=m-1;
-                r=m+1;
-            }
-        }
-        return ind;
-    }
+//     private int[] oddLongestPalindrome(String s){
+//         int maxi=1, m=1, l=0, r=2;
+//         int[] ind = new int[]{0,0};
+//         while(m<s.length()-1){
+//             if(l<0 || r == s.length()){
+//                 m++;
+//                 l=m-1;
+//                 r=m+1;
+//                 continue;
+//             }
+//             if(s.charAt(l)==s.charAt(r)){
+//                 if(maxi<r-l+1){
+//                     maxi = r-l+1;
+//                     ind[0]=l;
+//                     ind[1]=r;
+//                 }
+//                 l--;
+//                 r++;
+//             }
+//             else{
+//                 m++;
+//                 l=m-1;
+//                 r=m+1;
+//             }
+//         }
+//         return ind;
+//     }
     
-    private int[] evenLongestPalindrome(String s){
-        int maxi=1, m=1, l = m-1, r = m;
-        int[] ind = new int[]{0,0};
-        while(m<s.length()){
-            if(l<0 || r == s.length()){
-                m++;
-                l=m-1;
-                r=m;
-                continue;
-            }
-            if(s.charAt(l)==s.charAt(r)){
-                if(maxi<r-l+1){
-                    maxi = r-l+1;
-                    ind[0]=l;
-                    ind[1]=r;
-                }
-                l--;
-                r++;
-            }
-            else{
-                m++;
-                l=m-1;
-                r=m;
-            }
-        }
-        return ind;
-    }
+//     private int[] evenLongestPalindrome(String s){
+//         int maxi=1, m=1, l = m-1, r = m;
+//         int[] ind = new int[]{0,0};
+//         while(m<s.length()){
+//             if(l<0 || r == s.length()){
+//                 m++;
+//                 l=m-1;
+//                 r=m;
+//                 continue;
+//             }
+//             if(s.charAt(l)==s.charAt(r)){
+//                 if(maxi<r-l+1){
+//                     maxi = r-l+1;
+//                     ind[0]=l;
+//                     ind[1]=r;
+//                 }
+//                 l--;
+//                 r++;
+//             }
+//             else{
+//                 m++;
+//                 l=m-1;
+//                 r=m;
+//             }
+//         }
+//         return ind;
+//     }
 }
