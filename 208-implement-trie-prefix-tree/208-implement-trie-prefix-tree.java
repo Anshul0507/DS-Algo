24ae56cfacd1016f04530da33f79c9721821429e
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-09-08 10:09:04 +0530

    Time: 84 ms (28.43%), Space: 66.6 MB (75.33%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -2,7 +2,6 @@ class TrieNode {
     public char val;
     public boolean isWord; 
     public TrieNode[] children = new TrieNode[26];
-    public TrieNode() {}
     public TrieNode(char c){
         this.val = c;
     }
