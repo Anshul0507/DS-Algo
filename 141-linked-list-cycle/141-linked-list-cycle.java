d67caec4f8c9111c28c99b324bfeafc8c94f62cf
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-09-14 03:03:47 +0530

    Time: 3 ms (22.16%), Space: 45.5 MB (75.91%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,16 +11,16 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head==null || head.next==null)
-            return false;
-        ListNode temp = head;
-        int visMark = 1000000;
-        while(temp!=null){
-            if(temp.val == visMark)
-                return true;
-            temp.val=visMark;
-            temp = temp.next;
-        }
-        return false;
+        if (head == null || head.next == null) 
+			return false;
+		ListNode p = head;
+		ListNode q = head;
+		while(q != null && q.next != null) {
+			p = p.next;
+			q = q.next.next;
+			if (p == q)
+				return true;
+		}
+		return false;
     }
 }
