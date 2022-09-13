2ebd0baad69f8a022883ef2841ec269b394942c9
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-09-14 02:53:10 +0530

    Time: 2 ms (68.15%), Space: 50.3 MB (65.23%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,8 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int curr = nums[0];
-        for(int i=1;i<nums.length;i++)
-            curr ^= nums[i];
-        return curr;
+        int result = 0;
+        for(int i : nums) {
+            result ^= i;
+        }
+        return result;
     }
 }
