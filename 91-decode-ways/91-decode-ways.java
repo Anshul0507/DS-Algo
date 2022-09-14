d2a3a1b78daec754ed40a1ec48fa0dea80fc5dd3
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-09-15 01:15:24 +0530

    Time: 1 ms (98.72%), Space: 42.6 MB (30.70%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -6,9 +6,10 @@ class Solution {
         int[] count = new int[n+1];
         count[1]=count[0]=1;
         for(int i=2;i<=n;i++){
-            if(s.charAt(i-1)>'0')
+            char c1 = s.charAt(i-1), c2=s.charAt(i-2);
+            if(c1>'0')
                 count[i]=count[i-1];
-            if(s.charAt(i-2)=='1' || (s.charAt(i-2)=='2' && s.charAt(i-1)<'7'))
+            if(c2=='1' || (c2=='2' && c1<'7'))
                 count[i]+=count[i-2];
         }
         return count[n];
