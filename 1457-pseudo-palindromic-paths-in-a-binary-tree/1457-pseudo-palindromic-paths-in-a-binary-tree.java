2fc539e9cdedbff4d8e986c3b4655e66e2178885
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-09-14 12:53:40 +0530

    Time: 25 ms (63.74%), Space: 107.3 MB (58.19%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -16,27 +16,32 @@
 class Solution {
     int count =0;
     public int pseudoPalindromicPaths (TreeNode root) {
-        HashSet<Integer> pathSet = new HashSet<>();
-        checkPalindrome(root,pathSet);
+        int[] arr = new int[10];
+        checkPalindrome(root,arr);
         return count;
     }
     
-    private void checkPalindrome(TreeNode node, HashSet<Integer> pathSet){
-        if(pathSet.contains(node.val))
-            pathSet.remove(node.val);
-        else
-            pathSet.add(node.val);
+    private boolean isPalindrome(int[] arr){
+        int countOdd =0;
+        for(int n : arr){
+            if(n%2==1)
+                countOdd++;
+            if(countOdd>1)
+                return false;
+        }
+        return true;
+    }
+    
+    private void checkPalindrome(TreeNode node, int[] arr){
+        arr[node.val]++;
         if(node.left==null && node.right==null){
-            if(pathSet.size()<=1)
+            if(isPalindrome(arr))
                 count++;
         }
         if(node.left!=null)
-            checkPalindrome(node.left,pathSet);
+            checkPalindrome(node.left,arr);
         if(node.right!=null)
-            checkPalindrome(node.right,pathSet);
-        if(pathSet.contains(node.val))
-            pathSet.remove(node.val);
-        else
-            pathSet.add(node.val);
+            checkPalindrome(node.right,arr);
+        arr[node.val]--;
     }
 }
