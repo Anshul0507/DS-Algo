abadcc0fc03c9b7e64218c11a371a9e6fd937790
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-09-18 03:35:45 +0530

    Time: 12 ms (85.97%), Space: 41.8 MB (59.48%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -15,19 +15,21 @@ class Solution {
             return false;
         Arrays.sort(m);
         int[] sides = {side,side,side,side};
-        return makesquareHelper(n-1,m,sides);
+        return makesquareHelper(n-1,m,sides,side);
     }
     
-    private boolean makesquareHelper(int i, int[] m, int[] sides){
+    private boolean makesquareHelper(int i, int[] m, int[] sides, int side){
         if(i==-1)
             return sides[0]==sides[1] && sides[0]==sides[2] && sides[0]==sides[3];
         for(int x=0;x<4;x++){
             if(sides[x]<m[i])
                 continue;
             sides[x]-=m[i];
-            if(makesquareHelper(i-1,m,sides))
+            if(makesquareHelper(i-1,m,sides,side))
                 return true;
             sides[x]+=m[i];
+            if(sides[x]==side)
+                break;
         }
         return false;
     }
