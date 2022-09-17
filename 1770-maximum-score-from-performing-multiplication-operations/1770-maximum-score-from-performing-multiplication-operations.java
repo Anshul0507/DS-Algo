6de2851dcfdcdb8c9725d36f52a2c9a1294777c2
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-09-17 22:58:20 +0530

    Time: 281 ms (49.78%), Space: 160.6 MB (19.33%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,16 +1,20 @@
 class Solution {
-    public int maximumScore(int[] nums, int[] multipliers) {
-        Integer[][] mem = new Integer[multipliers.length][multipliers.length];
-        return solve(0,0,nums,multipliers,mem);
+    int n, m;
+    int[] nums, muls;
+    Integer[][] memo;
+    public int maximumScore(int[] nums, int[] muls) {
+        n = nums.length;
+        m = muls.length;
+        this.nums= nums;
+        this.muls = muls;
+        this.memo = new Integer[m][m];
+        return dp(0, 0);
     }
-    
-    private int solve(int l, int i, int[] nums, int[] multipliers, Integer[][] mem){
-        if(i==multipliers.length)
-            return 0;
-        if(mem[l][i]!=null)
-            return mem[l][i];
-        int leftMax = nums[l]*multipliers[i] + solve(l+1,i+1,nums,multipliers,mem);
-        int rightMax = nums[nums.length-(i-l)-1]*multipliers[i] + solve(l,i+1,nums,multipliers,mem);
-        return mem[l][i] = Math.max(leftMax,rightMax);
+    private int dp(int l, int i) {
+        if (i == m) return 0; // Picked enough m elements
+        if (memo[l][i] != null) return memo[l][i];
+        int pickLeft = dp(l+1, i+1) + nums[l] * muls[i]; // Pick the left side
+        int pickRight = dp(l, i+1) + nums[n-(i-l)-1] * muls[i]; // Pick the right side
+        return memo[l][i] = Math.max(pickLeft, pickRight);
     }
 }
