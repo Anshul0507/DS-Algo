107802728e6dd4c6740a51a6cb77bcc88f5bc938
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-09-17 23:10:38 +0530

    Time: 37 ms (94.31%), Space: 72.1 MB (86.54%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -1,19 +1,21 @@
 class Solution {
     public int maximumScore(int[] nums, int[] multipliers) {
-        if(multipliers.length==1){
-            return Math.max(nums[0]*multipliers[0],nums[nums.length-1]*multipliers[0]);
+        int[] dp = new int[nums.length + 1];
+        for (int m = multipliers.length - 1, delta = nums.length - multipliers.length; m >= 0; m--, delta++) {
+            for (int i = 0; i < nums.length - delta; i++) {
+                dp[i] = Math.max(dp[i] + multipliers[m] * nums[i + delta], dp[i + 1] + multipliers[m] * nums[i]);
+            }
         }
-        Integer[][] mem = new Integer[multipliers.length][multipliers.length];
-        return solve(0,0,nums,multipliers,mem);
+        return dp[0];
     }
     
-    private int solve(int l, int i, int[] nums, int[] multipliers, Integer[][] mem){
-        if(i==multipliers.length)
-            return 0;
-        if(mem[l][i]!=null)
-            return mem[l][i];
-        int leftMax = nums[l]*multipliers[i] + solve(l+1,i+1,nums,multipliers,mem);
-        int rightMax = nums[nums.length-(i-l)-1]*multipliers[i] + solve(l,i+1,nums,multipliers,mem);
-        return mem[l][i] = Math.max(leftMax,rightMax);
-    }
+    // private int solve(int l, int i, int[] nums, int[] multipliers, Integer[][] mem){
+    //     if(i==multipliers.length)
+    //         return 0;
+    //     if(mem[l][i]!=null)
+    //         return mem[l][i];
+    //     int leftMax = nums[l]*multipliers[i] + solve(l+1,i+1,nums,multipliers,mem);
+    //     int rightMax = nums[nums.length-(i-l)-1]*multipliers[i] + solve(l,i+1,nums,multipliers,mem);
+    //     return mem[l][i] = Math.max(leftMax,rightMax);
+    // }
 }
