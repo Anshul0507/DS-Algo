38722846f61930854661f2913b947c10a72ccee3
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-10-11 18:56:11 +0530

    Time: 15 ms (95.97%), Space: 136.7 MB (89.51%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,30 +1,45 @@
-class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        if(n==1 || source==destination)
-            return true;
-        List<Integer>[] adj = new ArrayList[n];
-        for(int i=0;i<n;i++)
-            adj[i]=new ArrayList<>();
-        for(int i=0; i< edges.length; i++){
-            int from = edges[i][0];
-            int to = edges[i][1];
-            adj[from].add(to);
-            adj[to].add(from);
+class DisjointSetUnion{
+    private int[] parent;
+    private int N;
+    
+    public DisjointSetUnion(int n){
+        this.N = n;
+        this.parent = new int[this.N];
+        for(int i = 0; i < this.N; i++){
+            this.parent[i] = i;
         }
-        boolean[] visited = new boolean[n];
-        return dfs(source,destination,visited,adj);
     }
     
-    private boolean dfs(int current, int destination, boolean[] visited, List<Integer>[] adj){
-        if(visited[current])
-            return false;
-        if(current==destination)
-            return true;
-        visited[current]=true;
-        List<Integer> neighbours = adj[current];
-        for(int neighbour : neighbours)
-            if(dfs(neighbour,destination,visited, adj))
-                return true;
-        return false;
+    public boolean areConnected(int u, int v){
+        return find(u) == find(v);
+    }
+    
+    public void union(int u, int v){
+        if(u != v){
+            int a = find(u);
+            int b = find(v);
+            parent[a] = b;
+        }
+    }
+    
+    private int find(int u){
+        int x = u;
+        while(x != this.parent[x]){
+            x = this.parent[x];
+        }
+        
+        this.parent[u] = x;
+        return x;
+    }
+}
+
+class Solution {
+    public boolean validPath(int n, int[][] edges, int start, int end) {
+        DisjointSetUnion set = new DisjointSetUnion(n);
+        for(int[] edge : edges){
+            set.union(edge[0], edge[1]);
+        }
+        
+        return set.areConnected(start, end);
     }
 }
