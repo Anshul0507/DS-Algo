06322b32a0c5009686e3f40dd80392a1f6e39b01
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-10-19 02:34:09 +0530

    Time: 2 ms (64.02%), Space: 41.9 MB (63.02%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)-'0' >= 0 && s.charAt(i)-'0' <=9){
+                int digit = s.charAt(i)-'0';
+                while(s.charAt(++i)!='['){
+                    digit = digit*10 + s.charAt(i)-'0';
+                }
+                i--;
+                st.push(digit);
+            }
+            else if(s.charAt(i)!=']')
+                sb.append(s.charAt(i));
+            else{
+                
+                int repitition = st.pop(), left = sb.length()-1;
+                while(sb.charAt(left)!='[')
+                    left--;
+                sb.deleteCharAt(left);
+                int right = sb.length();
+                for(int k = 1; k< repitition; k++){
+                    sb.append(sb.substring(left,right));
+                }
+                    
+            }
+        }
+        return sb.toString();
+    }
+}
