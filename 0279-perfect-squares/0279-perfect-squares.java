67b091d72e9dbee5c16aada588ea5746565f4e83
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-10-20 16:24:17 +0530

    Time: 2 ms (99.22%), Space: 41.6 MB (91.86%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,17 +1,32 @@
 class Solution {
     public int numSquares(int n) {
-        int[] dp = new int[n+1];
-       
-       Arrays.fill(dp, Integer.MAX_VALUE);
-       dp[0] = 0;
+      if(n == 0) return 0;
         
-       for(int i=1; i<=n; i++){
-               
-           for(int j=1; j<=Math.sqrt(i); j++){
-               dp[i] = Math.min(dp[i], (dp[i-(j*j)]+1));
-           }    
-       } 
+		// case 1:
+        if(isSqrt(n)) return 1;
         
-        return dp[n];
+		   // case 2 :If answer is 2 => one factor has to be (int)square_root and remaining other number has to be a perfect square
+        for(int i=1; i<=Math.sqrt(n); i++){
+            if(isSqrt(n-(i*i))) return 2;
+        }
+        
+		 // case 4: a positive integer can be expressed as the sum of three squares if and only if it is not of the form 4^k(8m+7) for some int k and m see "not of" in above line
+		 
+        // in order to pass 28 as 4
+        while(n%4 ==0)
+            n=n/4;
+        
+        if(n%8 == 7){
+            return 4;    // handling 4^k
+        }
+        
+		 // case 3
+        return 3;
+    }
+    
+    private boolean isSqrt(int n){
+        int sqrt = (int)Math.sqrt(n);
+        
+        return n-(sqrt*sqrt)==0;
     }
 }
