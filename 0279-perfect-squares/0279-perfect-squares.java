e10b6d2da40e845488ff1655bdc21c3bf3dd629e
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-10-21 02:47:58 +0530

    Time: 68 ms (50.54%), Space: 76.4 MB (13.53%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,27 +1,24 @@
 class Solution {
     public int numSquares(int n) {
-        if(n==Math.sqrt(n))
-            return 1;
-        Queue<Integer> q = new LinkedList();
-        HashSet<Integer> h = new HashSet<>();
-        q.add(n);
-        int count = 1;
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=1;i<=size;i++){
-                int curr = q.poll();
-                int sq = (int)Math.sqrt(curr);
-                if(curr==sq*sq)
-                    return count;
-                for(int j=1;j<=sq;j++){
-                    if(h.contains(curr-j*j))
-                        continue;
-                    q.add(curr-j*j);
-                    h.add(curr-j*j);
-                }   
-            }
-            count++;
-        }
-        return -1;
+        Queue<Integer> queue = new LinkedList<>();
+       Set<Integer> reviewedSet = new HashSet<>();
+         
+       if(n >0) queue.offer(n);  
+       int level = 0;
+       while(!queue.isEmpty()){
+           level++; 
+           int size = queue.size();
+           for(int i=0; i<size; i++) {
+               int val = queue.poll();
+               if(!reviewedSet.add(val)) continue;
+               
+               for(int j=1; j<=Math.sqrt(val); j++){
+                   if(val-(j*j) == 0) return level;
+                   queue.offer(val-(j*j));
+               }
+           }
+       }  
+        
+       return level;
     }
 }
