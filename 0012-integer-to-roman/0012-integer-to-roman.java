67ec351fd630f29a9512715d2da4dc82d6dd3c76
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-10-20 13:52:33 +0530

    Time: 10 ms (59.13%), Space: 45.6 MB (54.17%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -4,6 +4,11 @@ class Solution {
         String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
         String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
         String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+        StringBuilder sb = new StringBuilder();
+        sb.append(M[num/1000])
+            .append(C[(num%1000)/100])
+            .append(X[(num%100)/10])
+            .append(I[num%10]);
+        return sb.toString();
     }
 }
