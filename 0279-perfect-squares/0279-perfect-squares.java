2469f6ca39a6d9bd5577844bf78044eb9e220ef0
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-10-21 02:46:12 +0530

    Time: 84 ms (43.76%), Space: 68.1 MB (15.59%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,32 +1,27 @@
 class Solution {
     public int numSquares(int n) {
-      if(n == 0) return 0;
-        
-		// case 1:
-        if(isSqrt(n)) return 1;
-        
-		   // case 2 :If answer is 2 => one factor has to be (int)square_root and remaining other number has to be a perfect square
-        for(int i=1; i<=Math.sqrt(n); i++){
-            if(isSqrt(n-(i*i))) return 2;
+        if(n==Math.sqrt(n))
+            return 1;
+        Queue<Integer> q = new LinkedList();
+        HashSet<Integer> h = new HashSet<>();
+        q.add(n);
+        int count = 1;
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=1;i<=size;i++){
+                int curr = q.poll();
+                int sq = (int)Math.sqrt(curr);
+                if(curr==sq*sq)
+                    return count;
+                for(int j=1;j<=sq;j++){
+                    if(h.contains(curr-j*j))
+                        continue;
+                    q.add(curr-j*j);
+                    h.add(curr-j*j);
+                }   
+            }
+            count++;
         }
-        
-		 // case 4: a positive integer can be expressed as the sum of three squares if and only if it is not of the form 4^k(8m+7) for some int k and m see "not of" in above line
-		 
-        // in order to pass 28 as 4
-        while(n%4 ==0)
-            n=n/4;
-        
-        if(n%8 == 7){
-            return 4;    // handling 4^k
-        }
-        
-		 // case 3
-        return 3;
-    }
-    
-    private boolean isSqrt(int n){
-        int sqrt = (int)Math.sqrt(n);
-        
-        return n-(sqrt*sqrt)==0;
+        return -1;
     }
 }
