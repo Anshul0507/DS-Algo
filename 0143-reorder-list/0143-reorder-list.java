69d82dc6c3c129821ab51390038121e21d74b145
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-10-22 08:25:44 +0530

    Time: 11 ms (20.60%), Space: 52.6 MB (80.19%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -18,10 +18,16 @@ class Solution {
             fast = fast.next.next;
         }
         ListNode rev;
+        // list size is even, slow will be on the left middle node
+        // eg = List = 1,2,3,4
+        // first half = 1,2; second half = 3,4
         if(fast==null){
             rev = reverse(slow.next);
             slow.next = null;
         }
+        //list size is odd, so need to reverse the second half of list such that length of second half = length of first half -1
+        // eg = List = 1,2,3,4,5
+        // first half should be 1,2,3 -> second half = 4,5
         else{
             rev = reverse(slow.next.next);
             slow.next.next = null;
@@ -29,6 +35,7 @@ class Solution {
         join(head,rev);
     }
     
+    // function to reverse the list
     private ListNode reverse(ListNode node){
         ListNode prev = null, current = node, next = node;
         while(current!=null){
@@ -40,6 +47,7 @@ class Solution {
         return prev;
     }
     
+    // join node 1, node 2 such that node 1 length >= node 2 length
     private void join(ListNode node1, ListNode node2){
         ListNode temp = node1;
         while(temp!=null && node2!=null){
