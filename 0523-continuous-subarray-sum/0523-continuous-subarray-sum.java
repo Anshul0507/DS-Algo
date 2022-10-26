4b0ac8e0a48289bb236b6adf4a3f1e3d2530a5fc
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-10-26 16:17:20 +0530

    Time: 56 ms (63.89%), Space: 113.7 MB (36.73%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.java
@@ -1,19 +1,33 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-        int n = nums.length,sumSoFar=0;
+        //{0,5,1,5,5,0}
+        //{0,5,1,1,5,0}
+        int n = nums.length;
         if(n==1)
             return false;
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        hm.put(0,-1);
-        for(int i=0;i<n;i++){
+        // HashMap<Integer,Integer> hm = new HashMap<>();
+        // hm.put(0,-1);
+        // for(int i=0;i<n;i++){
+        //     sumSoFar+=nums[i];
+        //     sumSoFar%=k;
+        //     Integer prev = hm.get(sumSoFar);
+        //     if (prev != null) {
+        //         if (prev < i-1)
+        //             return true;
+        //     }
+        //     else hm.put(sumSoFar, i);
+        // }
+        // return false;
+        
+        //HashSet solution
+        HashSet<Integer> h = new HashSet<>();
+        h.add(0);
+        int sumSoFar=nums[0];
+        for(int i=1;i<n;i++){
             sumSoFar+=nums[i];
-            sumSoFar%=k;
-            Integer prev = hm.get(sumSoFar);
-            if (prev != null) {
-                if (prev < i-1)
-                    return true;
-            }
-            else hm.put(sumSoFar, i);
+            if(h.contains(sumSoFar%k))
+                return true;
+            h.add((sumSoFar-nums[i])%k);
         }
         return false;
     }
