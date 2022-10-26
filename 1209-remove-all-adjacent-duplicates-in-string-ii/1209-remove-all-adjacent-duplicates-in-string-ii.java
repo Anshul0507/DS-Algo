8408da13bfe7c60ee05f1dc62bfb5a7b149a192a
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-10-26 23:20:34 +0530

    Time: 13 ms (95.88%), Space: 43.1 MB (92.74%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,23 +1,13 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-        String res = "";
-        Stack<Pair<Character,Integer>> st = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            Pair<Character,Integer> prev = st.isEmpty()? null : st.peek();
-            if(null==prev || prev.getKey()!=s.charAt(i))
-                st.add(new Pair(s.charAt(i),1));
-            else{
-                if(prev.getValue()==k-1){
-                    for(int j=1;j<k;j++)
-                        st.pop();
-                }
-                else
-                    st.add(new Pair(s.charAt(i),prev.getValue()+1));
-            }
+        int[] count = new int[s.length()];
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            sb.append(c);
+            int last = sb.length()-1;
+            count[last] = 1 + (last > 0 && sb.charAt(last) == sb.charAt(last-1) ? count[last-1] : 0);
+            if(count[last] >= k) sb.delete(sb.length()-k, sb.length());
         }
-        while(!st.isEmpty()){
-            res = st.pop().getKey() + res;
-        }
-        return res;
+        return sb.toString();
     }
 }
