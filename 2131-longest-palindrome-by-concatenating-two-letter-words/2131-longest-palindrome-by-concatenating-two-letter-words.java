6a1110aa6099e6dfffef77e21a0fbf3614414bd9
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-11-04 01:38:36 +0530

    Time: 18 ms (94.95%), Space: 97.8 MB (71.63%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,34 +1,21 @@
 class Solution {
     public int longestPalindrome(String[] words) {
-        HashMap<String,Integer> hm = new HashMap<>();
         int[][] freq = new int[26][26];
         int count = 0;
         for(String w: words){
             int i = w.charAt(0)-'a', j = w.charAt(1)-'a';
-            if(i==j)
-                freq[i][j]++;
+            if(freq[j][i]>0){
+                count+=4;
+                freq[j][i]--;
+            }
             else{
-                //found a reverse for dislikes
-                if(freq[j][i]>0){
-                    count+=4;
-                    freq[j][i]--;
-                }
-                else{
-                    freq[i][j]++;
-                }
+                freq[i][j]++;
             }
         }
         boolean oddPresent = false;
-        for(int i=0;i<26;i++){
-            if(freq[i][i]%2==1){
-                oddPresent = true;
-                count+=(freq[i][i]-1)*2;
-            } 
-            else
-                count+=freq[i][i]*2;
-        }
-        if(oddPresent)
-            return count + 2;
+        for(int i=0;i<26;i++)
+            if(freq[i][i]>0)
+                return count + 2;
         return count;
     }
 }
