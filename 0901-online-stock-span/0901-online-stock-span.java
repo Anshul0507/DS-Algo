b07d822a34f6c437f264f223606266ae843320ab
Anshul Bajpai <anshulbajpai2013@gmail.com>
2022-11-10 02:54:08 +0530

    Time: 77 ms (49.84%), Space: 69.8 MB (67.53%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -7,8 +7,7 @@ class StockSpanner {
     
     public int next(int price) {
         int span = 1;
-        while(!st.isEmpty() && st.peek()[0] <= price)
-            span += st.pop()[1];
+        while(!st.isEmpty() && st.peek()[0] <= price) span += st.pop()[1];
         st.push(new int[]{price, span});
         return span;
     }
